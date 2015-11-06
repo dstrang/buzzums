2fada62f5da17b750a21421de8dc5ccfc85c2c4c
David Strang <david@weareflagship.com>
2015-11-06 00:45:22 -0800

    init commit

---
+++ server/app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = socket(server);
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+// app.post('/', function (req, res) {
+//   console.log('BUZZ BUZZ!');
+// });
+
+io.on('connection', function(client) {  
+    console.log('Client connected...');
+
+    client.on('buzz', function(data){
+    	console.log(data);
+    });
+
+});
+
+server.listen(3000, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Express server listening at http://%s:%s', host, port);
+});
